158966440f52e002b4f7ceb40dfa4c30d52397b1
pralay mondal <connect.pro24@gmail.com>
2020-07-12 11:50:04 +0530

    Google Captcha code using nodejs and express js

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+const port = 3000;
+app.get('/',function(req,res) {
+  // Sending our HTML file to browser.
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit',function(req,res){
+	console.log(req.connection.remoteAddress);
+  // g-recaptcha-response is the key that browser will generate upon form submit.
+  // if its blank or null means user has not selected the captcha, so return the error.
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
+  }
+
+  // Put your secret key here.
+  var secretKey = "6LdKcLAZAAAAAKH5mRvfOgxfPXeTwMtz4BrLwoq8";
+  // req.connection.remoteAddress will provide IP address of connected user.
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  // Hitting GET request to the URL, Google will respond with success or error scenario.
+  request(verificationUrl,function(error,response,body) {
+    body = JSON.parse(body);
+    // Success will be true or false depending upon captcha validation.
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    }
+    res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
+  });
+});
+
+// This will handle 404 requests.
+app.use("*",function(req,res) {
+  res.status(404).send("404");
+})
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
